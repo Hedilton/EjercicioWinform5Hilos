6b0a569505d37ac7b79a0cdeb7a5283afaf79935
Hedilton <hediltoncolana@gmail.com>
2021-06-11 15:54:56 -0500

    Agregar archivos de proyecto.

---
+++ EjercicioWinform5Hilos/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace EjercicioWinform5Hilos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Tabla11 = new System.Windows.Forms.ListBox();
+            this.Tabla12 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(223, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Tabla11
+            // 
+            this.Tabla11.FormattingEnabled = true;
+            this.Tabla11.Location = new System.Drawing.Point(55, 126);
+            this.Tabla11.Name = "Tabla11";
+            this.Tabla11.Size = new System.Drawing.Size(176, 277);
+            this.Tabla11.TabIndex = 1;
+            // 
+            // Tabla12
+            // 
+            this.Tabla12.FormattingEnabled = true;
+            this.Tabla12.Location = new System.Drawing.Point(292, 126);
+            this.Tabla12.Name = "Tabla12";
+            this.Tabla12.Size = new System.Drawing.Size(180, 277);
+            this.Tabla12.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 456);
+            this.Controls.Add(this.Tabla12);
+            this.Controls.Add(this.Tabla11);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox Tabla11;
+        private System.Windows.Forms.ListBox Tabla12;
+    }
+}
+
---
+++ EjercicioWinform5Hilos/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace EjercicioWinform5Hilos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread a = new Thread(metodo1);
+            a.Start();
+            Thread b = new Thread(metodo2);
+            b.Start();
+        }
+        public void metodo1()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                this.Tabla11.Items.Add("11 * " + i + " = " + 11 * i);
+                Thread.Sleep(200);
+            }
+        }
+        public void metodo2()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                this.Tabla12.Items.Add("12 * " + i + " = " + 12 * i);
+                Thread.Sleep(150);
+            }
+        }
+    }
+}
